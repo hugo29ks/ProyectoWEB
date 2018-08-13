16125a48794248808bff847bc6eb2669483435ae
Ronny Cabrera <ronny-38@hotmail.com>
2018-08-13 01:30:45 -0500

    Front End - Guardar Item Software

--- FrontEnd/src/app/ingresar-software/ingresar-software.component.ts
+++ FrontEnd/src/app/ingresar-software/ingresar-software.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import {ItemServicio} from "../servicios/item.servicio";
 
 @Component({
   selector: 'app-ingresar-software',
@@ -7,9 +8,23 @@ import { Component, OnInit } from '@angular/core';
 })
 export class IngresarSoftwareComponent implements OnInit {
 
-  constructor() { }
+  constructor(private _itemServicio: ItemServicio) { }
+
+  nombreSoftware = "";
+  fabricanteSoftware = "";
+  codigoLicencia = "";
+  arquitectura = "";
+  tiempoValidez = "";
+  adquisionSoftware = "";
+  proveedorSoftware = "";
+  empleadoSoftware = "";
+  comentarioSodtware = "";
 
   ngOnInit() {
+  }
+
+  guardar() {
+    this._itemServicio.saveSoftware(this.nombreSoftware, this.fabricanteSoftware, this.codigoLicencia, this.arquitectura, this.tiempoValidez, this.adquisionSoftware, this.proveedorSoftware, this.empleadoSoftware, this.comentarioSodtware).subscribe();
   }
 
 }
--- FrontEnd/src/app/servicios/item.servicio.ts
+++ FrontEnd/src/app/servicios/item.servicio.ts
@@ -1,5 +1,5 @@
 import {Injectable} from "@angular/core";
-import {HttpClient, HttpHeaders} from "@angular/common/http";
+import {HttpClient, HttpHeaders, HttpParams} from "@angular/common/http";
 import {UsuarioServicio} from "./usuario.servicio";
 
 @Injectable()
@@ -11,7 +11,7 @@ export class ItemServicio {
 
   static getCommonHeaders() {
     let headers = new HttpHeaders();
-    headers.append("Content-Type", "application/json");
+    headers.append("Content-Type", "application/json, application/x-www-form-urlencoded");
     headers.append("Access-Control-Allow-Origin", "*");
     headers.append("Access-Control-Allow-Methods", "POST, GET, OPTIONS");
     headers.append("Access-Control-Allow-Headers", "Content-Type, Access-Control-Allow-Headers, Authorization, X-Requested-With, Access-Control-Allow-Origin, Access-Control-Allow-Methods");
@@ -21,5 +21,22 @@ export class ItemServicio {
   getItem(element) {
     let header = UsuarioServicio.getCommonHeaders();
     return this.http.get(`http://localhost:3000/Item/${element}`, {headers: header});
+  }
+
+  saveSoftware(nombreSoftware:string, fabricanteSoftware:string, licencia:string, arquitectura:string, tiempoValidez:string, adquisionSoftware:string, proveedorSoftware:string, empleadoSoftware:string, comentarioSoftware:string) {
+    let header = UsuarioServicio.getCommonHeaders();
+    let body = new HttpParams();
+    body = body.set('elemento', "software");
+    body = body.set('nombreItem', nombreSoftware.toString());
+    body = body.set('fabricante', fabricanteSoftware.toString());
+    body = body.set('licencia', licencia.toString());
+    body = body.set('arquitectura', arquitectura.toString());
+    body = body.set('tiempoValidez', tiempoValidez.toString());
+    body = body.set('anioAdquisicion', adquisionSoftware.toString());
+    body = body.set('proveedor', proveedorSoftware.toString());
+    body = body.set('empleado', empleadoSoftware.toString());
+    body = body.set('comentario', comentarioSoftware.toString());
+
+    return this.http.post(`http://localhost:3000/Item/`, body, {headers: header});
   }
 }
